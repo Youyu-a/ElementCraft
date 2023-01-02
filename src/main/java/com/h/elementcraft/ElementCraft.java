4f911b84dc812a0e9f32d6ea32726f64d4e40686
mituz <mituzhizhong@outlook.com>
2023-01-02 23:54:15 +0800

    Start

--- src/main/java/com/h/elementcraft/ElementCraft.java
+++ src/main/java/com/h/elementcraft/ElementCraft.java
@@ -1,4 +1,4 @@
-package net.fabricmc.example;
+package com.h.elementcraft;
 
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
--- src/main/java/com/h/elementcraft/mixin/ElementCraftMixin.java
+++ src/main/java/com/h/elementcraft/mixin/ElementCraftMixin.java
@@ -1,6 +1,6 @@
-package net.fabricmc.example.mixin;
+package com.h.elementcraft.mixin;
 
-import net.fabricmc.example.ElementCraft;
+import com.h.elementcraft.ElementCraft;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -8,7 +8,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
-public class ExampleMixin {
+public class ElementCraftMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
 		ElementCraft.LOGGER.info("This line is printed by an example mod mixin!");
